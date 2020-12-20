37a017536569719cb4557bf55cbe711c349852ae
kleevs <31158070+kleevs@users.noreply.github.com>
2020-12-20 18:19:19 +0100

    Update ioc.tsx

--- src/tools/ioc.tsx
+++ src/tools/ioc.tsx
@@ -1,22 +1,22 @@
-import React, { Context } from 'react';
+// import React, { Context } from 'react';
 
-export type Collection = { 
-    add: <T>(context: Context<T>) => {
-        value: (value: T) => void;
-    }
-}
+// export type Collection = { 
+//     add: <T>(context: Context<T>) => {
+//         value: (value: T) => void;
+//     }
+// }
 
-type Config = { (collection: Collection): void };
+// type Config = { (collection: Collection): void };
 
-export default function IoC({config, children}: {config: Config, children: any}) {
-    const collections: { context: Context<any>, value: any }[] = [];
-    config({
-        add: (context) => ({
-            value: (value) => collections.push({ context, value })
-        })
-    });
+// export default function IoC({config, children}: {config: Config, children: any}) {
+//     const collections: { context: Context<any>, value: any }[] = [];
+//     config({
+//         add: (context) => ({
+//             value: (value) => collections.push({ context, value })
+//         })
+//     });
 
-    let root = <>{children || ''}</>;
-    collections.forEach(({ context: { Provider }, value }) => root = <Provider value={value}>{root}</Provider>);
-    return root;
-}
+//     let root = <>{children || ''}</>;
+//     collections.forEach(({ context: { Provider }, value }) => root = <Provider value={value}>{root}</Provider>);
+//     return root;
+// }
