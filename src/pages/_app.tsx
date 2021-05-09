e771d35bab31f2034b41e2794ea6532390206f72
Kevin Landa <kev.sam.landa@gmail.com>
2021-05-09 13:50:20 +0200

    styled component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,12 @@ function Meta({children}) {
         <link href='/styles/bootstrap.css' rel='stylesheet' type='text/css' />
         <link href='/styles/toastr.css' rel='stylesheet' type='text/css' />
         <link href='/styles/sb-admin-2.css' rel='stylesheet' type='text/css' />
+        <style dangerouslySetInnerHTML={{__html: `
+          html, body, #__next { 
+            width: 100%;
+            height: 100%;
+          }
+        `}} />
         <script src="/scripts/jquery.js" />
         <script src="/scripts/bootstrap.js" />
         <script src="/scripts/sb-admin-2.js" />
---
+++ src/pages/get-started.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Content, Header, Menu, Wrapper } from 'src/components/ui/layout';
+
+
+export default function Page() {
+  return <Wrapper>
+    <Header>Header</Header>
+    <Menu>Menu</Menu>
+    <Content>Content</Content>
+  </Wrapper>
+}
