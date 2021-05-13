7b11bca0f286c1e4d1b5f5b57b89e55422971bfb
Kevin Landa <kev.sam.landa@gmail.com>
2021-05-13 18:34:46 +0200

    signin component

--- packages/auth/src/component/signin.tsx
+++ packages/auth/src/component/signin.tsx
@@ -1,47 +1,36 @@
-import type { preventDefault } from 'lib/src/main'
+import type { Textfield, preventDefault } from 'lib/src/main'
 import type signin from '../action/signin'
 import React from 'react';
+import styled from 'styled-components';
 
 type Deps = {
+    Textfield: typeof Textfield;
     preventDefault: typeof preventDefault;
     signin: (typeof signin) extends (...arg) => infer T ? T : typeof signin;
 }
 
-export default ({ preventDefault, signin }: Deps) => 
-function SigninComponent({ value, onChange }: Components.Signin.Props) {
-    return <div className="container">
-        <div className="row justify-content-center">
-            <div className="col-xl-10 col-lg-12 col-md-9">
-                <div className="card o-hidden border-0 shadow-lg my-5">
-                    <div className="card-body p-0">
-                        <div className="row">
-                            <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div className="col-lg-6">
-                                <div className="p-5">
-                                    <div className="text-center">
-                                        <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                                    </div>
-                                    <form className="user" onSubmit={(e) => preventDefault(e, () => signin(value?.login, value?.password))}>
-                                        <div className="form-group">
-                                        <input type="email" value={value?.login} onChange={(e) => onChange({ ...value, login: e.target.value })} className="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..."/>
-                                        </div>
-                                        <div className="form-group">
-                                        <input type="password" value={value?.password} onChange={(e) => onChange({ ...value, password: e.target.value })} className="form-control form-control-user" id="exampleInputPassword" placeholder="Password"/>
-                                        </div>
-                                        <div className="form-group">
-                                        <div className="custom-control custom-checkbox small">
-                                        </div>
-                                        </div>
-                                        <button type="submit" className="btn btn-primary btn-user btn-block">
-                                        Login
-                                        </button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+const Container = styled.div`
+    display: grid;
+    grid-gap: 10px;
+`
+
+const Form = styled.form`
+    padding: 1.2rem;
+`
+type Value = {
+    login: string;
+    password: string;
+}
+
+export default ({ Textfield, preventDefault, signin }: Deps) => 
+function SigninComponent({ value, onChange }: {
+    value: Value; 
+    onChange: (value: Value) => void;
+}) {
+    return <Form onSubmit={(e) => preventDefault(e, () => signin(value.login, value.password))}>
+        <Container>
+            <Textfield data-id='login' placeholder='Email' value={value.login} onChange={(login) => onChange({ ...value, login })} />
+            <Textfield data-id='password' placeholder='Password' value={value.password} onChange={(password) => onChange({ ...value, password })} />
+        </Container>
+    </Form>
 }
--- packages/auth/src/main.ts
+++ packages/auth/src/main.ts
@@ -1,8 +1,8 @@
-import { post, notifySuccess as success, notifyError as error, preventDefault } from 'lib/src/main'
+import { post, notifySuccess as success, notifyError as error, preventDefault, Textfield } from 'lib/src/main'
 import signinServiceFactory from './service/signin'
 import signinFactory from './action/signin'
 import signinComponentFactory from './component/signin'
 
 const signinService = signinServiceFactory({ post })
 const signin = signinFactory({ signinService, success, error })
-export const signinComponent = signinComponentFactory({ preventDefault, signin })
+export const SigninComponent = signinComponentFactory({ preventDefault, signin, Textfield })
